9ccd9f0ce46e3f149284f5b9b33802d2140d575f
fiordor <1fiordor@gmail.com>
2021-09-16 16:21:10 +0200

    ip publica conseguida

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,39 @@
 
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const public_ip = '82.223.68.210';
 const port = 3000;
 
+/*
 const net = require('net');
 const client = net.connect({ port: 80, host: "google.com" }, () => {
   console.log('MyIP=' + client.localAddress);
   console.log('MyPORT=' + client.localPort);
 });
+*/
 
 const mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'admin',
-  password: 'Pensando_En_Voz_Baja_v1#',
+  password: fs.readFileSync('./adminPass.txt', {encoding:'utf8'} ),
   database: 'blog'
 });
 
 connection.connect();
 
-connection.query('SELECT * FROM article', function (err, rows, fields) {
-  if (err) throw err;
-  console.log('Primera consulta');
-  console.log(rows);
+app.get('/', (req, res) => {
+  res.send('Nice try');
 });
 
-app.get('/', (req, res) => {
+app.get('/articles', (req, res) => {
   connection.query('SELECT * FROM article', function (err, rows, fields) {
     if (err) throw err;
     res.send(rows);
   });
-  //res.send('Hello World!');
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, public_ip, () => {
+  console.log(`Example app listening at http://${public_ip}:${port}`);
 });
-
-// MENSAJE DE SINCRONIZACION

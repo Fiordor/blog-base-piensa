7632b723258fa369b8de6a0bd9d768bd46de2753
Fiordor <1fiordor@gmail.com>
2021-09-21 09:07:38 +0200

    hicendo el login en back

--- backend/src/manager.js
+++ backend/src/manager.js
@@ -1,19 +1,44 @@
 
 module.exports.run = (req, res, con) => {
 
-    if (req.body['fun'] == undefined) {
-        res.send( { res: 'err', message: 'fun is not defined' } );
-    }
+	if (req.body['fun'] == undefined) {
+		res.send({ res: 'err', message: 'fun is not defined' });
+	}
 
-    switch (req.body.fun) {
-        case 'getAllArticles': getAllArticles(req, res, con); break;
-        default: res.send( { res: 'err', message: 'fun not exists' } );
-    }
+	switch (req.body.fun) {
+		case 'login': login(req, res, con); break;
+		case 'getAllArticles': getAllArticles(req, res, con); break;
+		default: res.send({ res: 'err', message: 'fun not exists' });
+	}
 }
 
 function getAllArticles(req, res, con) {
-    con.query('SELECT * FROM articles', (err, rows, fields) => {
-        if (err) { res.send(err); return; }
-        res.send(rows);
-    });
+	let sql = 'SELECT * FROM articles';
+	con.query(sql, (err, rows, fields) => {
+		if (err) { res.send(err); return; }
+		res.send(rows);
+	});
+}
+
+/**
+ * Login usando el nickname y el password
+ * req.body = { fun: string, user: User }
+ */
+function login(req, res, con) {
+
+	if (req.body['user'] == undefined) {
+		res.send({ res: 'err', message: 'user not exists' });
+		return;
+	}
+
+	let reqUser = req.body.user;
+	if (reqUser['nickname'] == undefined) {
+		res.send({ res: 'err', message: 'nickname not exists' });
+		return;
+	}
+
+	let sql = `SELECT * FROM users u WHERE u.nickname = '${reqUser.nickname}';`;
+	con.query(sql, (err, rows, fields) => {
+
+	});
 }
